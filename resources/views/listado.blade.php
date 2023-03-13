7feda2dd51632bce14f3be752235192bfc9a9f17
RodrigoTG04 <rodrigo.torres.ag961204@gmail.com>
2023-03-13 16:05:45 -0600

    jurado

--- resources/views/listado.blade.php
+++ resources/views/listado.blade.php
@@ -4,7 +4,7 @@
     <div  class="container">
       <div>
       <center>@include('parciales.intro')</center>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
       <hr>
       </div>
       <div>
